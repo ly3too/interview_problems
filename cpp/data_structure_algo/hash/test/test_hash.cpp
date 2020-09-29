34982f0267c41de742071c77988e7b590619d142
ly3too <ly3too@qq.com>
2020-09-29 07:35:12 +0100

    add hash function

--- cpp/data_structure_algo/hash/test/test_hash.cpp
+++ cpp/data_structure_algo/hash/test/test_hash.cpp
@@ -1,7 +1,36 @@
 #include "hash/hash.hpp"
 #include "gtest/gtest.h"
+#include <functional>
 
 using namespace std;
+using namespace my_hash;
+
+enum class Enu1 {
+    DEFAULT = 0,
+};
+
+TEST(test, hash) {
+    auto val1 = 1;
+    auto val2 = 2;
+    cout << "MyHash(val1) = " << MyHash(val1) << endl;
+    ASSERT_EQ(MyHash(val1), std::size_t(val1));
+    ASSERT_EQ(MyHash(val2), std::size_t(val2));
+    ASSERT_EQ(MyHash(&val1), std::size_t(&val1));
+
+    Enu1 enu = Enu1::DEFAULT;
+    ASSERT_EQ(MyHash(enu), std::size_t(enu));
+
+    string str1 = "abc";
+    string str2 = str1;
+    cout << "MyHash(str1) = " << MyHash(str1) << endl;
+    ASSERT_EQ(MyHash(str1), MyHash(str2));
+    ASSERT_EQ(MyHash(str1.c_str(), str1.size()), MyHash(str2));
+
+    cout << "MyHash<DJB2Hash>(str1) = " << MyHash<string, DJB2Hash>(str1) << endl;
+    ASSERT_EQ((MyHash<string, DJB2Hash>(str1)), (MyHash<string, DJB2Hash>(str2)));
+
+    cout << "MyHash(\"abcd\", 4) = " << MyHash("abcd", 4) << endl;
+}
 
 int main() {
     testing::InitGoogleTest();
